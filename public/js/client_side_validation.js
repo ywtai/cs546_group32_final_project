ccf993f2f4276fb10c62fc073ca14bea5d7dc8bc
unknown <ywtai0219@gmail.com>
2024-05-04 17:26:33 -0400

    add review css

--- public/js/client_side_validation.js
+++ public/js/client_side_validation.js
@@ -605,6 +605,30 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   })
 });
+
+//review photo slides
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
 // favorite-btn AJAX
 document.addEventListener('DOMContentLoaded', function () {
         const favoriteButtons = document.querySelectorAll('.favorite-btn');
@@ -720,3 +744,4 @@ function toggleLike(button) {
     })
     .catch(error => console.error('Error:', error));
 }
+
